2a7048445610993585770a5681f4383685cb081c
Anton Scherbakov <ashcherbakov.work@yandex.ru>
2022-11-02 18:31:08 +0900

    corrected

--- src/com/asherbakov/drivers/DriverCategoryB.java
+++ src/com/asherbakov/drivers/DriverCategoryB.java
@@ -1,56 +1,15 @@
 package com.asherbakov.drivers;
 
 import com.asherbakov.transport.Car;
-import com.asherbakov.transport.enums.RightsCategory;
 
-public class DriverCategoryB<T extends Car> {
-    final String NO_NAME = "Водитель пожелал остаться неизвестным";
-    private String fullName;
-    private RightsCategory rightsCategory;
-    private int experience;
+public class DriverCategoryB<T extends Car> extends Driver {
 
     public DriverCategoryB(String fullName, int experience) {
-        if (fullName != null && !fullName.isBlank()) {
-            this.fullName = fullName;
-        } else {
-            this.fullName = NO_NAME;
-        }
-        setExperience(experience);
+        super(fullName, experience);
     }
 
-    public void start() {
-        System.out.println("Начало движения");
+    public void race(T transport) {
+        System.out.println(String.format("Водитель %s управляет автомобилем '%s' и будет участвовать в заезде.", getFullName(), transport.toString()));
     }
 
-    public void stop() {
-        System.out.println("Остановка");
-    }
-
-    public void fuel() {
-        System.out.println("Заправить автомобиль");
-    }
-
-    public void race(T car) {
-        System.out.println(String.format("Водитель %s управляет автомобилем '%s' и будет участвовать в заезде.", getFullName(), car.toString()));
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public RightsCategory getRightsCategory() {
-        return rightsCategory;
-    }
-
-    public int getExperience() {
-        return experience;
-    }
-
-    public void setExperience(int experience) {
-        if (experience > 0) {
-            this.experience = experience;
-        } else {
-            this.experience = 1;
-        }
-    }
 }
